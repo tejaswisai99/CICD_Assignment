4f6f2bfcc0962a904d3e94fd94aa9860e09840e1
Josyula Sai Tejaswi <tejaswisai@IISc.ac.in>
2024-04-07 01:39:38 +0530

    train success and test gets good score

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, StackingClassifier
 import pickle
 import numpy as np
 
@@ -8,7 +9,17 @@ X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
-model = LogisticRegression().fit(X, y)
+
+base_models = [
+    ('rf', RandomForestClassifier(n_estimators=100, random_state=42)),
+    ('lr', LogisticRegression(random_state=42))
+]
+
+meta_model = LogisticRegression(random_state=42)
+
+stacking_ensemble = StackingClassifier(estimators=base_models, final_estimator=meta_model, cv=5)
+
+model = stacking_ensemble.fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
